3e5cb6caf0cc7bc2c8d213c355e583d866845365
phanendraguptha <www.phani122@gmail.com>
2020-04-18 20:50:28 +0530

    added api

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
 else{
   document.getElementById("change-theme-btn").src = "./images/sun.png";
 }
+
+// weather api call
+var climateCondition = document.querySelector('.climateCondition');
+var temperature = document.querySelector('.temp');
+const wind = document.querySelector('.winds');
+const humidity = document.querySelector('.humidity');
+
+window.onkeyup = keyup;
+//creates a global Javascript variable
+function keyup(e) {
+  //listens for you to press the ENTER key, at which point your web address will change to the one you have input in the search box
+  if (e.keyCode == 13) {
+    var search = document.getElementsByClassName("input")[0].value;
+    search = String(search);
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=daef535c29750b1a31263719f56e4d1a')
+    .then(res => res.json())
+    .then(data => {
+      climateCondition.innerHTML = data.weather[0].description;
+      temperature.innerHTML = (data.main.temp-272.15).toFixed(2) + ' °c';
+      wind.innerHTML = data.wind.speed + ' m/s';
+      humidity.innerHTML = data.main.humidity;
+    })
+    .catch(err => {
+      alert("kindly enter city name correctly");
+    })
+  }
+}
